0a8cfb615121c2b9cd75805643aefbab258e0eb5
Phongsathron Kittiworapanya <phongsathron@outlook.com>
2018-07-31 11:59:03 +0700

    Change to crop image from array

--- crop.py
+++ crop.py
@@ -1,11 +1,11 @@
 import cv2
 
-def crop(path, posx1, posy1, posx2, posy2):
+def crop(image, posx1, posy1, posx2, posy2):
     """ Crop Function take path, x1, y1, x2, y2 then give back cropped photo """
 
-    data = cv2.imread(path)
+    # data = cv2.imread(path)
 
-    cropped = data[posy1: posy1 + abs(posy2 - posy1), posx1: posx1 + abs(posx2 - posx1)]
+    cropped = image[posy1: posy1 + abs(posy2 - posy1), posx1: posx1 + abs(posx2 - posx1)]
 
     return cropped
 
