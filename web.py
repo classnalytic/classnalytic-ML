fefb15596b7f53b3eed2805e3d36e5cd1c639a48
Wiput Pootong <wiput.pootong@gmail.com>
2018-10-17 00:37:04 +0700

    :sparkles: Add worker~!

---
+++ web.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from celery import Celery
+import subprocess
+
+app = Flask("classnalytic")
+
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+@celery.task(name='classnalytic.model_train')
+def model_train():
+    returned = subprocess.check_output("make train")
+    return returned.decode("utf-8")
